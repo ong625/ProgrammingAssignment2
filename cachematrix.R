f53cc747c49ea766affa975dc7358cb0beb0fd86
ong625 <shirnee.ong@gmail.com>
2020-08-03 12:04:48 +0800

    git commit -a -m

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The code helps to invert matrices number input
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  r <- NULL
+  set <- function(y){
+    x <<- y
+    r<<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) r <<- inverse
+  getInverse <- function() r 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Get the mean from the cache tp calculate the final mean 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  r<- x$getInverse()
+  if(!is.null(r)){
+    message("getting cached data")
+    return(r)
+  }
+  mat <- x$get()
+  r <- solve(mat,...)
+  x$setInverse(r)
+  j
+  }
